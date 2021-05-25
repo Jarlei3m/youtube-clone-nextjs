a78fef51f6fce3237f79eaa537c0d91efd8fe7d1
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-05-25 19:09:38 -0300

    style: Material-ui config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Button } from '@material-ui/core'
 
 export default function Home() {
   return (
-    <div>home</div>
+    <div>
+      home
+      <Button variant='outlined' color='secondary'>Hello world</Button>
+    </div>
   )
 }
---
+++ src/pages/theme/index.ts
@@ -0,0 +1,21 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { colors } from '@material-ui/core';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#f44336',
+    },
+    secondary: {
+      main: '#3ea6ff',
+    },
+    background: {
+      default: colors.common.white,
+      dark: "#f4f6f8",
+      paper: colors.common.white
+    },
+  },
+});
+
+export default theme;
