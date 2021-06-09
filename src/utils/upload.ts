0d321422cdaf6fbe1cb2911ada9dfc21df7f2858
Jarlei <jarlei_rodrigues@outlook.com>
2021-06-09 10:46:32 -0300

    chore: Changed env variables

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -10,9 +10,9 @@ declare const region: string;
 declare const bucket: string;
 
 aws.config.update({
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  secretAccessKey: process.env.AWSSECRET_KEY,
+  accessKeyId: process.env.AWSACCESS_KEY,
+  region: process.env.AWSREGION,
 });
 
 const s3 = new aws.S3({
@@ -22,7 +22,7 @@ const s3 = new aws.S3({
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: process.env.AWS_BUCKET,
+    bucket: process.env.AWSBUCKET,
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req: NextApiRequest, file: any, cb: any) {
